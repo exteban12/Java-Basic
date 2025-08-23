3c513b8b3db2b7301f872cf07e579623677a1666
ESTEBAN <ESTEBAN@DESKTOP-8LO4HGC>
2025-08-22 19:24:54 -0500

    prueba

--- src/cadena/EjerciosCadena.java
+++ src/cadena/EjerciosCadena.java
@@ -9,33 +9,39 @@ package cadena;
  * @author ESTEBAN
  */
 public class EjerciosCadena {
+
     public static void main(String[] args) {
-        
+
         String cadena1 = "Hola";
         String cadena2 = "Mundo";
-        String Cadena3 = "Esteban Robles Perez";
+        String cadena3 = "Esteban Robles Perez";
+        
         // 1. Concatena dos cadenas de texto.
         System.out.println(cadena1 + " " + cadena2);
-        
+        System.out.println("----------------------------------------------------");
+
         // 2. Muestra la longitud de una cadena de texto.
-        System.out.println(Cadena3.length());
+        System.out.println(cadena1.length());
+
+        System.out.println(cadena2.length());
+
+        System.out.println(cadena3.length());
+        System.out.println("----------------------------------------------------");
 
         // 3. Muestra el primer y último carácter de un string.
+        System.out.println(cadena1.charAt(0));
+        System.out.println(cadena1.charAt(3));
+        System.out.println("----------------------------------------------------");
 
         // 4. Convierte a mayúsculas y minúsculas un string.
-
+        System.out.println(cadena1.toUpperCase());
+        System.out.println(cadena2.toLowerCase());
         // 5. Comprueba si una cadena de texto contiene una palabra concreta.
-
         // 6. Formatea un string con un entero.
-
         // 7. Elimina los espacios en blanco al principio y final de un string.
-
         // 8. Sustituye todos los espacios en blanco de un string por un guión (-).
-
         // 9. Comprueba si dos strings son iguales.
-
         // 10. Comprueba si dos strings tienen la misma longitud.
-        
     }
-    
+
 }
