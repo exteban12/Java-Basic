59bbca0bd5145089df6635a9d351b94ae6940e45
ESTEBAN <ESTEBAN@DESKTOP-8LO4HGC>
2025-09-14 16:12:28 -0500

    Commit desde intelij

--- src/Operadores/Operadores.java
+++ src/Operadores/Operadores.java
@@ -6,6 +6,7 @@ public class Operadores {
 
         int a = 6;
         int b = 6;
+        int c = 1;
 
         System.out.println(a + b);
         System.out.println(a - b);
@@ -16,15 +17,16 @@ public class Operadores {
         // asignacion
         a -= 1;
         System.out.println(a);
-        a += 1;
-        System.out.println(a);
+        c += 1;
+        System.out.println(c);
         a *= 2;
         System.out.println(a);
         a /= 2;
         System.out.println(a);
         a %= 2;
         System.out.println(a);
-        
+                System.out.println("_____________________");
+
         
         // operadores de comparacion
         
