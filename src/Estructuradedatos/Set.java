dc5afb862d568841bb1adc7cfadc9ca395fa72f2
ESTEBAN <ESTEBAN@DESKTOP-8LO4HGC>
2025-09-14 16:16:19 -0500

    Commit desde intelije con una la nueva clase set

---
+++ src/Estructuradedatos/Set.java
@@ -0,0 +1,17 @@
+
+package Estructuradedatos;
+
+
+import java.util.HashSet;
+
+public class Set {
+    public static void main(String[] args) {
+        
+        HashSet<String> name = new HashSet<>();
+
+
+
+        
+    }
+    
+}
